5b10e7d75cb42e527bf0a155848b35685e803ebc
Mokhutso <manaswemahlako@gmail.com>
2024-01-31 10:54:09 +0200

    db

--- todo/settings.py
+++ todo/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%t4bx5hpksq45bq+-jlp(yojp=vadoyt=jx=*&q+ukk5wusohg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 # Load environment variables from .env file
 load_dotenv()
